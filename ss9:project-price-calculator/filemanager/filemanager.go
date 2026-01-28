0884ffa2e38abb315a5fe7864667f758ddcd7486
longps <longsydn1105@gmail.com>
2026-01-28 14:15:13 +0700

    FEAT: ad func write func

--- ss9:project-price-calculator/filemanager/filemanager.go
+++ ss9:project-price-calculator/filemanager/filemanager.go
@@ -2,6 +2,7 @@ package filemanager
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"os"
 )
@@ -27,4 +28,23 @@ func ReadLines(path string) ([]string, error) {
 
 	file.Close()
 	return lines, nil
+}
+
+func WriteJSON(path string, data any) error {
+	file, err := os.Create(path)
+
+	if err != nil {
+		return errors.New("ERROR: Fail to create file")
+	}
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data)
+
+	if err != nil {
+		file.Close()
+		return errors.New("ERROR: Fail to convert data to JSON")
+	}
+
+	file.Close()
+	return nil
 }
--- ss9:project-price-calculator/prices/prices.go
+++ ss9:project-price-calculator/prices/prices.go
@@ -10,7 +10,7 @@ import (
 type TaxIncludedPriceJob struct {
 	TaxRate          float64
 	InputPrices      []float64
-	TaxIncludePrices map[string]float64
+	TaxIncludePrices map[string]string
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
@@ -29,13 +29,14 @@ func (job *TaxIncludedPriceJob) LoadData() {
 func (job *TaxIncludedPriceJob) Process() {
 	job.LoadData()
 	result := make(map[string]string)
-	
+
 	for _, price := range job.InputPrices {
 		taxIncluedPrice := price * (1 + job.TaxRate)
 		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncluedPrice)
 	}
 
-	fmt.Println(result)
+	job.TaxIncludePrices = result
+	filemanager.WriteJSON(fmt.Sprintf("result_%.0f.json", job.TaxRate*100), job)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
