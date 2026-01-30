85e61e68d35b8b1b8f0bebe47f15c5ab7ab1df1d
longps <longsydn1105@gmail.com>
2026-01-30 14:10:31 +0700

    FEAT: create test api

---
+++ ss11:buid-rest-api/db/db.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	DB, err := sql.Open("sqlite3", "api.db")
+
+	if err != nil {
+		panic("Could not connet Database")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+
+	createTable()
+}
+
+func createTable() {
+	createEventTable := `
+		CREATE TABLE IF NET EXISTS events(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			description TEXT NOT NULL,
+			location TEXT NOT NULL,
+			dateTime DATETIME NOT NULL,
+			user_id INTEGER
+		)
+	`
+
+	_, err := DB.Exec(createEventTable)
+
+	if err != nil {
+		panic("Could not create events table")
+	}
+}
